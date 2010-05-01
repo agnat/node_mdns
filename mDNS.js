dca072b9f5c2ef7ea4e0a9caff7c5581c4479d5b
David Siegel <david.siegel@artcom.de>
2010-05-02 00:24:23 +0200

    added semicolons

--- mDNS.js
+++ mDNS.js
@@ -3,18 +3,21 @@ binding = require('./binding');
 var Service = binding.Service;
 Service.prototype.announce = function() {
   return this.doAnnounce("_" + this.name + "._" + this.protocol, this.port);
-}
+};
 
 exports.createService = function(name, port, protocol, ready_callback) {
   var service = new Service();
   service.name = name;
   service.port = port;
-  service.protocol = protocol
+  service.protocol = protocol;
   if (ready_callback) {
     service.addListener('ready', ready_callback);
   }
   return service;
-}
+};
+
+exports.createServiceBrowser = function() {
+};
 
 exports.TCP = "tcp";
 exports.UDP = "udp";
