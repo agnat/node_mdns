124e440ccf64e72faf7c9135683e3147df49a5c5
David Siegel <david.siegel@artcom.de>
2010-05-03 02:49:10 +0200

    emit serviceUp and serviceDown events added some tests

--- mDNS.js
+++ mDNS.js
@@ -19,6 +19,18 @@ exports.createAdvertisement = function(type, port, protocol, ready_callback) {
 
 //=== Service Browser ==========================================================
 
+function resolve(service_info, handler) {
+  var resolver = new Resolver();
+  resolver.addListener('resolved', function(error, resolver_info) {
+    for (var property in resolver_info) {
+      service_info[property] = resolver_info[property];
+    }
+    handler(service_info);
+    resolver.stop();
+  });
+  resolver.start(service_info)
+}
+
 var Browser = binding.Browser;
 Browser.prototype.start = function() {
   return this.doStart('_' + this.type + '._' + this.protocol);
@@ -28,6 +40,15 @@ exports.createBrowser = function(type, protocol) {
   var browser = new Browser();
   browser.type = type;
   browser.protocol = protocol;
+  browser.addListener('changed', function(err, flags, info) {
+    if (flags & binding.kDNSServiceFlagsAdd) {
+      resolve(info, function(resolver_info) {
+        browser.emit('serviceUp', resolver_info);
+      });
+    } else {
+      browser.emit('serviceDown', info);
+    }
+  });
   return browser;
 };
 
@@ -40,19 +61,7 @@ Resolver.prototype.start = function(service_info) {
       service_info.interface_index, 0);
 }
 
-exports.createResolver = function() {
-  return new Resolver();
-}
-
-exports.resolve = function(service_info, handler) {
-  var resolver = new Resolver();
-  resolver.addListener('resolved', function(error, resolver_info) {
-    handler(resolver_info);
-    resolver.stop();
-  });
-  resolver.start(service_info)
-}
-
+exports.resolve = resolve;
 //=== Constants ================================================================
 
 exports.TCP = "tcp";
--- test.js
+++ test.js
@@ -6,20 +6,23 @@ var puts = sys.puts,
     p    = sys.p;
 
 ad =  mDNS.createAdvertisement("node_js_mdns_test", 4321, mDNS.TCP);
-ad.start();
-
 browser = mDNS.createBrowser("node_js_mdns_test", mDNS.TCP);
-browser.addListener('changed', function(err, flags, info) {
-  var verb = 'gone';
-  if (flags & mDNS.ServiceAdd) {
-    verb = 'found';
-    mDNS.resolve(info, function(resolver_info) {
-      p("=== resolved", resolver_info);
-    });
-  }
-  puts('=== service ' + verb + ': ' + info.name + ' ' + info.regtype + ' ' + info.domain);
-});
-browser.start();
 
-setTimeout(function() { ad.stop(); }, 4000);
-setTimeout(function() { browser.stop(); }, 6000);
+timeout_id = setTimeout(function() { assert.fail('time out'); }, 10000);
+
+browser.addListener('serviceUp', function(info) {
+  p('Up',info);
+  assert.equal('_node_js_mdns_test._tcp.', info['regtype']);
+  assert.equal(4321, info['port']);
+  ad.stop();
+});
+
+browser.addListener('serviceDown', function(info) {
+  p('Down',info);
+  assert.equal('_node_js_mdns_test._tcp.', info['regtype']);
+  browser.stop();
+  clearTimeout(timeout_id);
+});
+
+browser.start();
+ad.start();
