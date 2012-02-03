a2fddeb2e4495cf5b3f8fd160d3a14bfaf1049a1
David Siegel <david.siegel@artcom.de>
2012-02-03 22:06:36 +0100

    [tests] clear timeout when done

--- tests/test_odd_ends.js
+++ tests/test_odd_ends.js
@@ -5,22 +5,22 @@ var mdns = require('../lib/mdns')
 
 exports['DNSServiceEnumerateDomains'] = function(t) {
   var enumerator = new mdns.MDNSService();
+  var timeout = setTimeout(function() {
+    console.log('[SKIPPED] probably running on ahavi');
+    enumerator.stop();
+    t.done()
+  }, 10000);
   function on_enum(sref, flags, iface, error, domain, context) {
     t.strictEqual(error, mdns.kDNSServiceErr_NoError, "error must be NoError");
     t.strictEqual(typeof flags, 'number', "'flags' must be of type number");
     t.strictEqual(typeof iface, 'number', "'iface' must be of type number");
 
     enumerator.stop();
-
+    clearTimeout(timeout);
     t.done();
   }
   mdns.dns_sd.DNSServiceEnumerateDomains(enumerator.serviceRef,
       mdns.kDNSServiceFlagsBrowseDomains, 0, on_enum, {some: 'context'});
   enumerator.start();
-  setTimeout(function() {
-    console.log('[SKIPPED] probably running on ahavi');
-    enumerator.stop();
-    t.done()
-  }, 10000);
 }
 
